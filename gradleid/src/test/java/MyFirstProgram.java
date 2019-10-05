7e4b5a21a260a009a8b74f7d85cba6d3e0453f96
Nazar Vistak <vistak.nazar@gmail.com>
2019-10-05 22:10:14 +0300

    Додано декілька функцій

--- gradleid/src/test/java/MyFirstProgram.java
+++ gradleid/src/test/java/MyFirstProgram.java
@@ -1,5 +1,20 @@
 public class MyFirstProgram {
     public static void main(String[] args){
-        System.out.println("hello world!");
+        hello("world");
+        hello("user");
+        double l=5;
+        double a=4;
+        double b=5;
+        System.out.println("Площа квадрату зі стороною" + l + "=" + area(l) );
+        System.out.println("Площа прямокутника =" + area(a,b));
+    }
+    public static void hello(String somebody) {
+        System.out.println("Hello " + somebody + "!");
+    }
+    public static double area(double l) {
+        return l*l;
+    }
+    public static double area(double a, double b) {
+        return a*b;
     }
 }
