5be09074c839ae504dcb92484dbe002a29957d80
Nazar Vistak <vistak.nazar@gmail.com>
2019-10-06 21:26:25 +0300

    Перший тест

---
+++ gradleid/src/test/java/JavaTests/SquareTests.java
@@ -0,0 +1,15 @@
+package JavaTests;
+
+import JavaLearning.Square;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class SquareTests {
+  @Test
+  public void testArea(){
+    Square s = new Square(5);
+    Assert.assertEquals(s.area(),25.0);
+  }
+}
